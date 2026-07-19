0e3eb46e04b83e02377e7274d7d307c8055e55ab
Evan Tobin <me@evantobin.com>
2026-07-18 23:30:12 -0400

    Fix PentairPump init to use heap allocation; strip to Pentair + Jandy only

--- main/pump/pump_protocol.cpp
+++ main/pump/pump_protocol.cpp
@@ -6,89 +6,62 @@
 static const char *TAG = "pump_proto";
 
 PumpProtocol pumpProtocol = {};
-static PentairPump sPentairPump;
-static uint8_t sJandyPumpId = JANDY_PUMP_ID_MIN;
+
+static PentairPump *sPentairPump = nullptr;
 
 // ---- Pentair wrappers ----
 
-static void pentairBegin(int txPin, int rxPin, uint8_t address) {
-  sPentairPump.begin(9600, txPin, rxPin, address);
-}
-
-static void pentairSetPower(bool on) {
-  if (on) {
-    sPentairPump.setRemoteControl(true);
-    sPentairPump.setPower(true);
-  } else {
-    sPentairPump.setPower(false);
-  }
-}
-
-static void pentairSetRpm(uint16_t rpm) {
-  sPentairPump.setRemoteControl(true);
-  sPentairPump.setRpm(rpm);
-}
-
-static void pentairSetGpm(uint8_t gpm) {
-  sPentairPump.setRemoteControl(true);
-  sPentairPump.setGpm(gpm);
-}
-
-static void pentairRequestStatus() {
-  sPentairPump.requestStatus();
-}
-
-static bool pentairReadStatus(uint16_t &rpm, uint16_t &watts, uint8_t &flow, bool &fault) {
-  PentairPumpStatus status;
-  if (!sPentairPump.readStatus(status)) return false;
-  rpm = status.rpm;
-  watts = status.watts;
-  flow = status.flow;
-  fault = status.pumpError != 0;
+static bool pInitPentair(int txPin, int rxPin, uint8_t address) {
+  if (sPentairPump) { delete sPentairPump; sPentairPump = nullptr; }
+  sPentairPump = new PentairPump(UART_NUM_1, address, 16);
+  if (!sPentairPump) return false;
+  sPentairPump->begin(9600, rxPin, txPin);
   return true;
 }
 
-static void pentairPoll() {
-  PentairPumpStatus s;
-  sPentairPump.readStatus(s);
+static void pSetPowerPentair(bool on) {
+  if (!sPentairPump) return;
+  if (on) { sPentairPump->setRemoteControl(true); sPentairPump->setPower(true); }
+  else sPentairPump->setPower(false);
 }
+
+static void pSetRpmPentair(uint16_t rpm) {
+  if (!sPentairPump) return;
+  sPentairPump->setRemoteControl(true);
+  sPentairPump->setRpm(rpm);
+}
+
+static void pSetGpmPentair(uint8_t gpm) {
+  if (!sPentairPump) return;
+  sPentairPump->setRemoteControl(true);
+  sPentairPump->setGpm(gpm);
+}
+
+static void pReqStatusPentair() { if (sPentairPump) sPentairPump->requestStatus(); }
+
+static bool pReadStatusPentair(uint16_t &rpm, uint16_t &watts, uint8_t &flow, bool &fault) {
+  if (!sPentairPump) return false;
+  PentairPumpStatus status;
+  if (!sPentairPump->readStatus(status)) return false;
+  rpm = status.rpm; watts = status.watts; flow = status.flow; fault = status.pumpError != 0;
+  return true;
+}
+
+static void pPollPentair() { if (sPentairPump) { PentairPumpStatus s; sPentairPump->readStatus(s); } }
 
 // ---- Jandy wrappers ----
 
-static void jandyBegin(int txPin, int rxPin, uint8_t address) {
-  if (address >= JANDY_PUMP_ID_MIN && address <= JANDY_PUMP_ID_MAX) {
-    sJandyPumpId = address;
-  }
-  jandyPumpBegin(UART_NUM_2, txPin, rxPin, sJandyPumpId);
-}
-
-static void jandySetPower(bool on) {
-  jandySetRpm(on ? 1800 : 0);
-}
-
-static void jandySetRpm(uint16_t rpm) {
-  jandySetRpm(rpm);
-}
-
-static void jandySetGpm(uint8_t /*gpm*/) {
-  jandySetRpm(2600);
-}
-
-static void jandyRequestStatus() {}
-
-static bool jandyReadStatus(uint16_t &rpm, uint16_t &watts, uint8_t &flow, bool &fault) {
+static void pSetPowerJandy(bool on) { ::jandySetRpm(on ? 1800 : 0); }
+static void pSetRpmJandy(uint16_t rpm) { ::jandySetRpm(rpm); }
+static void pSetGpmJandy(uint8_t) { ::jandySetRpm(2600); }
+static void pReqStatusJandy() {}
+static bool pReadStatusJandy(uint16_t &rpm, uint16_t &watts, uint8_t &flow, bool &fault) {
   JandyPumpStatus status;
-  if (!jandyReadStatus(status)) return false;
-  rpm = status.rpm;
-  watts = status.watts;
-  flow = 0;
-  fault = false;
+  if (!::jandyReadStatus(status)) return false;
+  rpm = status.rpm; watts = status.watts; flow = 0; fault = false;
   return true;
 }
-
-static void jandyPoll() {
-  jandyPoll();
-}
+static void pPollJandy() { ::jandyPoll(); }
 
 // ---- Public ----
 
@@ -96,28 +69,25 @@ void pumpProtocolInit(PumpProtocolType type, uint8_t pumpAddress, int txPin, int
   switch (type) {
     case PumpProtocolType::Pentair:
       pumpProtocol.type = type;
-      pumpProtocol.begin = pentairBegin;
-      pumpProtocol.setPower = pentairSetPower;
-      pumpProtocol.setRpm = pentairSetRpm;
-      pumpProtocol.setGpm = pentairSetGpm;
-      pumpProtocol.requestStatus = pentairRequestStatus;
-      pumpProtocol.readStatus = pentairReadStatus;
-      pumpProtocol.poll = pentairPoll;
+      pumpProtocol.setPower = pSetPowerPentair;
+      pumpProtocol.setRpm = pSetRpmPentair;
+      pumpProtocol.setGpm = pSetGpmPentair;
+      pumpProtocol.requestStatus = pReqStatusPentair;
+      pumpProtocol.readStatus = pReadStatusPentair;
+      pumpProtocol.poll = pPollPentair;
+      pInitPentair(txPin, rxPin, pumpAddress);
       ESP_LOGI(TAG, "Pentair pump protocol selected");
-      break;
+      return;
     case PumpProtocolType::Jandy:
       pumpProtocol.type = type;
-      pumpProtocol.begin = jandyBegin;
-      pumpProtocol.setPower = jandySetPower;
-      pumpProtocol.setRpm = jandySetRpm;
-      pumpProtocol.setGpm = jandySetGpm;
-      pumpProtocol.requestStatus = jandyRequestStatus;
-      pumpProtocol.readStatus = jandyReadStatus;
-      pumpProtocol.poll = jandyPoll;
+      pumpProtocol.setPower = pSetPowerJandy;
+      pumpProtocol.setRpm = pSetRpmJandy;
+      pumpProtocol.setGpm = pSetGpmJandy;
+      pumpProtocol.requestStatus = pReqStatusJandy;
+      pumpProtocol.readStatus = pReadStatusJandy;
+      pumpProtocol.poll = pPollJandy;
+      jandyPumpBegin(UART_NUM_2, txPin, rxPin, pumpAddress >= JANDY_PUMP_ID_MIN ? pumpAddress : JANDY_PUMP_ID_MIN);
       ESP_LOGI(TAG, "Jandy pump protocol selected");
-      break;
-  }
-  if (pumpProtocol.begin) {
-    pumpProtocol.begin(txPin, rxPin, pumpAddress);
+      return;
   }
 }
